60881ab039ba82b249bf823c9b6d9598b66ac98a
javel20 <javier_el_balla@hotmail.com>
2017-06-05 21:25:22 -0500

    BD16

--- database/seeds/TrabajadorTableSeeder.php
+++ database/seeds/TrabajadorTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use hosp\Tipotrabajador;
 use Illuminate\Database\Seeder;
 
 class TrabajadorTableSeeder extends Seeder
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use hosp\User;
+
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
